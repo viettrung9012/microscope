c9f8d18f015be67f3543bd86b31cb9436c2327c3
Truong Viet Trung <viettrung9012@yahoo.com>
2015-06-15 16:38:59 +0800

    page 253

---
+++ client/helpers/spacebars.js
@@ -0,0 +1,8 @@
+Template.registerHelper('pluralize', function(n, thing) {
+	// fairly stupid pluralizer
+	if (n <= 1) {
+		return n + ' ' + thing;
+	} else {
+		return n + ' ' + thing + 's';
+	}
+});
